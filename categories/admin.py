38429d069f2db36e10f689540696d16716fa52eb
Alish Shrestha <cresthaalish@gmail.com>
2025-09-04 16:07:35 +0545

    chore: add admin pages

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "parent", "created_at")
+    list_filter = ("parent", "created_at")
+    search_fields = ("name", "description")
+    prepopulated_fields = {"slug": ("name",)}
